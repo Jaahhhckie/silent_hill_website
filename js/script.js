ececf04bbbd10d7de44b1455cff18ca9b1985fd1
Jackie <Ardiles.Jackeline@hotmail.com>
2024-12-05 15:56:39 -0500

    edits to the music html and music css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 
 const albumLinks = {
-    'Silent Hill 1': 'https://youtu.be/mdqNshgDwtQ?si=qkZs3pHIx3hs8QF5',
+    'Silent Hill 1': 'https://youtube.com/playlist?list=PLvknaMBlC64hztXIhZZf-ZQueCNLJAB6w&si=MYSg2iA3e6zHSzhS',
     'Silent Hill 2': 'https://youtube.com/playlist?list=OLAK5uy_lCmbD75XUtYnmI1rKLabzho0dL9tr-L_Y&si=nUKbJQeOG3FrHzHZ',
     'Silent Hill 3': 'https://youtube.com/playlist?list=OLAK5uy_nS6fCOiLrPp2y377TR36fMi_TRLSolB84&si=aYMTQl70yrrU3bQG',
     'Silent Hill 4': 'https://youtube.com/playlist?list=OLAK5uy_kkB9YddR0GvOtJWSK1NEOFATDzvnqbcXk&si=td8BbtsGiyMsHRHU',
+    'Silent Hill: Origins': 'https://youtube.com/playlist?list=PLOEAs04auvicnFmMWlbfCnk8s_ovb9EPV&si=sE6HHLMrz0v6Zcvq',
+    'Silent Hill: Shattered Memories': 'https://youtube.com/playlist?list=PLE1EF37C25DEF0761&si=lMxPxZwLGK-No8y1',
 };
 
 function openYouTube(album) {
